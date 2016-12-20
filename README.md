b187eaab0b6a943489bd809e842e79ab67183ac6
BeltranBot <carlosbeltrane@gmail.com>
2016-12-19 23:34:11 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # vcustomers
-vue.js front end customers app
+
+> Simple customer manager front end
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, checkout the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+This front-end app is meant to work with this [slimapp RESTful API](https://github.com/BeltranBot/slimapp)
---
+++ src/components/Alert.vue
@@ -0,0 +1,25 @@
+<template>
+	<div v-if="message.status==='error'"class="alert alert-danger alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		{{message.text}}
+	</div>
+	<div v-else class="alert alert-success alert-dismissible" role="alert">
+		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		{{message.text}}
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'alert',
+  props: ['message'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
